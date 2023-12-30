37fbbba373d3f14ba93c7e67553d9c55996929c0
Clinton Jules <jules.clinton17@gmail.com>
2023-12-30 16:52:12 -0500

    Train and model python files

---
+++ train.py
@@ -0,0 +1,25 @@
+from fastai.vision.all import *
+
+def main(filepath: str, modelpath: str, show_results:bool = True):
+    # Define the path to your images
+    path = Path(filepath)
+
+    # Load your data
+    dls = ImageDataLoaders.from_folder(path, train='.', valid_pct=0.2, item_tfms=Resize(224))
+
+    # Create a learner
+    learn = vision_learner(dls, resnet34, metrics=error_rate)
+
+    # Train the model
+    learn.fine_tune(4)
+
+    # Evaluate and Export
+    if show_results:
+        learn.show_results()
+        plt.show()
+        
+    learn.export(modelpath)
+    
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2])

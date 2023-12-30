22126d42fd5ddbfdd90870b1ffdf3d1c0b2a0f29
Clinton Jules <jules.clinton17@gmail.com>
2023-12-30 15:40:18 -0500

    New data and update preprocess

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import cv2
 import json
 from bitmap_decode import base64_2_mask, overlay_mask
@@ -20,9 +21,13 @@ def pair_mask_2_img(annotation_path, image_path):
             origin = objects[0]['bitmap']['origin']
             
             # mask_image_pairing.append((image_path + image, base64_2_mask(mask), origin))
-            image = image_file_2_pixels(image_path + image)
+            image_pixels = image_file_2_pixels(image_path + image)
+            normalized_image = normalize_image(image_pixels)
+            
+            # Include image_shape in the tuple
+            image_shape = normalized_image.shape
                      
-            mask_image_pairing.append((normalize_image(image), base64_2_mask(mask), origin))
+            mask_image_pairing.append((normalized_image, base64_2_mask(mask), origin, image_shape))
 
         bitmap.close()
     
@@ -48,3 +53,17 @@ def annotate_images(mask_image_pairs_list):
         annotated_images.append(result_image)
         
     return annotated_images
+
+def align_mask(mask, origin, image_shape):
+    aligned_mask = np.zeros(image_shape[:2], dtype=np.float32)  # Create a blank mask with the same size as the image
+    x_start, y_start = origin
+
+    # Ensure the mask does not exceed the image boundaries
+    y_end = min(y_start + mask.shape[0], image_shape[0])
+    x_end = min(x_start + mask.shape[1], image_shape[1])
+
+    # Adjust mask dimensions if it extends beyond the image
+    mask = mask[:y_end - y_start, :x_end - x_start]
+
+    aligned_mask[y_start:y_end, x_start:x_end] = mask  # Place the mask on the blank canvas
+    return aligned_mask
